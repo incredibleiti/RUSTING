5944179e3b2c252ea3cd028b51dd4eb8a3546616
Iti Tyagi <intechworx@gmail.com>
2025-03-29 09:52:53 +0100

    Adding a small add function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn add(value1:i32, value2:i32)->i32{
+   return value1 + value2;
 }
+
+fn main() {
+    let x = 5; 
+    let y = 9;
+    let sum = add(x, y);
+    
+    println!("Now the value is: {sum}");
+}
+
