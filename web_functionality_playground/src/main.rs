2c4bbe9b55cfc3c627dac8505dcdcedf89371961
Iti Tyagi <intechworx@gmail.com>
2026-04-30 17:38:03 +0200

    Adding basic async code

---
+++ web_functionality_playground/src/main.rs
@@ -0,0 +1,11 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    println!("Listening on http://127.0.0.1:3000");
+    
+    axum::serve(listener, app).await.unwrap();
+}
