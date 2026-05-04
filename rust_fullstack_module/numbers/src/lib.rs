181857b93d3bc54b419117947d80758d8dc0e59e
Iti Tyagi <intechworx@gmail.com>
2026-05-04 12:14:27 +0200

    adding changes for lib for print array and vector

--- rust_fullstack_module/numbers/src/lib.rs
+++ rust_fullstack_module/numbers/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn say_hello() {
     println!("Hi this a call from library");
 }
+
+pub fn print_number_array() {
+    let numbers = [1, 2, 3, 4, 5];
+    for n in numbers.iter() {
+        println!("{}", n);
+    }
+}
+
+pub fn print_number_vector() {
+    let numbers = vec![5, 6, 7, 8, 9];
+    for n in numbers {
+        println!("{}", n);
+    }
+}
--- rust_fullstack_module/numbers/src/main.rs
+++ rust_fullstack_module/numbers/src/main.rs
@@ -2,4 +2,6 @@
 fn main() {
     //crate::module::function
     numbers::say_hello(); //calling sequence test
+    numbers::print_number_array();
+    numbers::print_number_vector();
 }
