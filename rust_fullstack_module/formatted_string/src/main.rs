38909911925812732457dc02b9785b9ab23fa595
Iti Tyagi <intechworx@gmail.com>
2026-05-07 14:11:47 +0200

    adding the base formatting

--- rust_fullstack_module/formatted_string/src/main.rs
+++ rust_fullstack_module/formatted_string/src/main.rs
@@ -9,4 +9,11 @@ fn main() {
 
     /* can also initialize the strings in a println */
     println!("{a} {b} {c}", a = "this", b="is", c = "a test." );
+
+    /* different formatted format to be called? */
+    let x = 11071988;
+    println!("{}", x); //base 10
+    println!("{:b}", x); //binary
+    println!("{:o}", x); //octal
+    println!("{:x}", x); //hexadecimal
 }
